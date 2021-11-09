1a98cf2741a447acdabec2f9ce53912ec794a8c2
samuelerg63 <samuelerg63@gmail.com>
2021-11-09 14:30:10 -0500

    update

--- lib/widgets/quiz_page.dart
+++ lib/widgets/quiz_page.dart
@@ -11,23 +11,57 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
 
+    List<Icon> scoreKeeper = const [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+  ];
+
+
+    Widget textButton (Color color, String texto, Icon icono){  
+
+       return TextButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(color),
+      ),
+      onPressed: () {
+        setState(() {
+          scoreKeeper.add(icono);
+        });
+        //iconos.scoreKeeper.add(this.icono);
+      },
+      child: Text(
+        texto,
+        style: const TextStyle(
+            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+      ),
+    );};
+
+  
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         _Texto(),
-        _Caja(
-          color: Colors.green,
-          texto: 'True',
-          icono: Icon(Icons.check),
-        ),
-        _Caja(
-          color: Colors.red,
-          texto: 'False',
-          icono: Icon(Icons.close),
-        ),
+        textButton(Colors.green, 'BIEN', Icon(Icons.check)),
+        SizedBox(height: 5,),
+        textButton(Colors.red, 'Mal', Icon(Icons.close)),
         Row(
+
           children: scoreKeeper,
         ),
       ],
@@ -57,73 +91,3 @@ class _Texto extends StatelessWidget {
   }
 }
 
-class _Caja extends StatefulWidget {
-  final Color color;
-  final String texto;
-  final Icon icono;
-
-  const _Caja({
-    Key? key,
-    required this.color,
-    required this.texto,
-    required this.icono,
-  });
-
-  @override
-  State<_Caja> createState() => _CajaState();
-}
-
-class _CajaState extends State<_Caja> {
-  @override
-  Widget build(BuildContext context) {
-    final textButton = TextButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(this.widget.color),
-      ),
-      onPressed: () {
-        setState(() {
-          
-          
-
-        });
-        //iconos.scoreKeeper.add(this.icono);
-      },
-      child: Text(
-        this.widget.texto,
-        style: const TextStyle(
-            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
-      ),
-    );
-
-    return Expanded(
-      child: Padding(
-        padding: EdgeInsets.all(15.0),
-        child: textButton,
-      ),
-    );
-  }
-}
-
-Icon lista (){
-
-  List scoreKeeper = [];
-  return List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-  ];
-
-}
