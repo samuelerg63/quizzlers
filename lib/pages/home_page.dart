92ca592333f1d09d08c396004bcab6068d65af1e
samuelerg63 <samuelerg63@gmail.com>
2021-11-09 21:25:21 -0500

    actctualizacion sin errores

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:quizzler_app/widgets/quiz_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
